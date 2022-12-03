c3d0c27f49618d96456ca78405abe465d605413d
J <equus497@gmail.com>
2022-12-02 18:27:52 -0700

    Added rename function with user input matching. Reconfigured struct to take Sting types instead of &str Implemented options, & match clauses for readability

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,38 @@
 use csv;
-use serde::Serialize;
+use serde::{Serialize, Deserialize};
 use std::path::PathBuf;
 use walkdir::WalkDir;
 use matroska::{Matroska, Settings::{Video, Audio}};
 
-#[derive(Debug, Serialize)]
-pub struct Movie <'a> {
-    title: &'a str,
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "UPPERCASE")]
+pub struct Movie {
+    title: String,
     year: i16,
     size: String,
     duration: String,
-    res: &'a str,
-    bit_depth: &'a str,
-    v_codec: &'a str,
-    a_codec: &'a str,
-    channels: f32,
-    encoder: &'a str,
+    res: i16,
+    bit_depth: String,
+    v_codec: String,
+    a_codec: String,
+    channels: String,
+    encoder: Option<String>,
     remux: bool,
+}
+
+trait TupleToStrings {
+    fn to_strings(&self) -> (String, String);
+}
+
+impl<T1: Copy + Into<String>, T2: Copy + Into<String>> TupleToStrings for (T1, T2) {
+    fn to_strings(&self) -> (String, String) {
+        (self.0.into(), self.1.into())
+    }
 }
 
 fn main() {
 
-    let start = std::time::Instant::now();
+    let timer = std::time::Instant::now();
     
     // Create a vector of all .mkv files at depth=2
     let mut directories = Vec::new();
@@ -41,18 +52,70 @@ fn main() {
         eprintln!("{}", e)
     }
 
-    let total_time = start.elapsed();
+    let total_time = timer.elapsed();
     println!("\nSuccessfully logged {} movies and exported to movie_log.csv in {:.4?}.", directories.len(), total_time);
 
-    // ! Use to generate an example matroska type
-    // let f = std::fs::File::open(&directories[99]).unwrap();
-    // let matroska = Matroska::open(&f).unwrap();
-    // println!("{:?}", matroska);
+    let mut input = String::new();
+    println!("Would you like to rename the folders? (Y/N)");
+    std::io::stdin().read_line(&mut input).expect("Failed to read input");
 
-    // Press Enter to quit
-    println!("Press Enter to exit...");
-    std::io::stdin().read_line(&mut String::new()).unwrap();
+    match input.to_lowercase().trim().chars().next().unwrap() {
+        'y' => {
+            if let Err(err) = rename() {
+                println!("{}", err);
+                std::process::exit(1);
+            }
+        },
+        _ => {println!("YEYE")}
+    }
 
+    // ! Press Enter to quit
+    // println!("Press Enter to exit...");
+    // std::io::stdin().read_line(&mut String::new()).unwrap();
+}
+
+fn rename() -> Result<(), Box<dyn std::error::Error>> {
+
+    let file = std::fs::File::open("D:/Movies/movie_log.csv")?;
+    let mut rdr = csv::Reader::from_reader(file);
+
+    let mut new_names = Vec::new();
+
+    for result in rdr.deserialize() {
+        let e: Movie = result?;
+
+        let channels = match e.channels.as_str() {
+            "2.0" => String::from("stereo"),
+            "1.0" => String::from("mono"),
+            num => format!("{}", num),
+        };
+
+        let encoder = match e.encoder{
+            Some(e) => format!(" {}",e),
+            None => String::new(),
+        };
+
+        let new_name = format!("{} ({}) [{}p {} {} {}-{}{}] ({} GB)", e.title, e.year, e.res, e.v_codec, e.bit_depth, e.a_codec, channels, encoder, e.size);
+
+        new_names.push(new_name);
+    }
+
+    let paths = std::fs::read_dir("M:/").unwrap();
+
+    if std::fs::read_dir("M:/").unwrap().count() == new_names.len() {
+        for (i, path) in paths.enumerate() {
+
+            let old_name = path.unwrap().path();
+            let new_name = format!("M:/{}", new_names[i]);
+
+            println!("{} » {}", old_name.display(), new_name);
+            std::fs::rename(old_name, new_name)?;
+        }
+    println!("Successfully renamed {} folders.", new_names.len());
+    }else {
+        println!("Found different number of files and folders.")
+    }
+    Ok(())
 }
 
 fn get_csv(directories: &Vec<PathBuf>) -> Result<(), Box<dyn std::error::Error>> {
@@ -67,22 +130,16 @@ fn get_csv(directories: &Vec<PathBuf>) -> Result<(), Box<dyn std::error::Error>>
         // Title
         let file_title = item.to_str().unwrap();
         let paren1 = &file_title.find("(").unwrap();
-        let title = &file_title[3..*paren1-1];
+        let title = String::from(&file_title[3..*paren1-1]);
 
         // Year
         let paren2 = &file_title.find(")").unwrap();
         let year_str = &file_title[*paren1+1..*paren2];
         let year = year_str.parse::<i16>().unwrap();
 
-        //Encoder & Remux
-        let has_encoder = get_encoder(&file_title);
-        let encoder = match has_encoder {
-            Some(enc) => enc,
-            None => "None"
-        };
-
+        // Encoder & Remux status
+        let encoder = get_encoder(&file_title);
         let remux = file_title.to_lowercase().contains("remux");
-        println!("{}", remux);
 
         // Size    » API returns number of bytes, must be converted
         let byte_count = std::fs::metadata(item).unwrap().len();
@@ -93,22 +150,21 @@ fn get_csv(directories: &Vec<PathBuf>) -> Result<(), Box<dyn std::error::Error>>
         let duration = get_dur(dur_secs);
 
     // ? VIDEO METADATA            
-        
         let vid_info = &matroska.tracks[0];
     
         // Resolution
-        let res = match &vid_info.settings {
-            Video(n) if n.pixel_width > 1920 => "2160p",
-            Video(n) if n.pixel_width <= 1920 => "1080p",
-            _ => "9999p",
+        let res: i16 = match &vid_info.settings {
+            Video(n) if n.pixel_width > 1920 => 2160,
+            Video(n) if n.pixel_width <= 1920 => 1080,
+            _ => panic!("Could not find resolution!"),
         };
 
         // Video Codec & Bit depth
         let (v_codec, bit_depth) = match vid_info.codec_id.as_str() {
             "V_MPEGH/ISO/HEVC" => ("x265", "10bit"),
             "V_MPEG4/ISO/AVC" => ("x264", "8bit"),
-            _ => ("XXXXXXXXXX", "XXXXXXXXXX")
-        };
+            _ => panic!("Could not find video codec!")
+        }.to_strings();
 
     // ? AUDIO METADATA
         // Audio codec
@@ -121,37 +177,35 @@ fn get_csv(directories: &Vec<PathBuf>) -> Result<(), Box<dyn std::error::Error>>
             "A_DTS" => "DTS",
             "A_TRUEHD" => "TrueHD Atmos",
             _ => "XXX",
-        };
+        }.to_string();
 
         // Audio Channels
-        let channels: f32 = match &aud_info.settings {
-            Audio(c) if c.channels == 8 => 7.1,
-            Audio(c) if c.channels == 7 => 6.1,
-            Audio(c) if c.channels == 6 => 5.1,
-            Audio(c) if c.channels == 4 => 4.0,
-            Audio(c) if c.channels == 2 => 2.0,
-            Audio(c) if c.channels == 0 => 1.0,
-            _ => 9.9,
-        };
+        let channels: String = match &aud_info.settings {
+            Audio(c) if c.channels == 8 => "7.1",
+            Audio(c) if c.channels == 7 => "6.1",
+            Audio(c) if c.channels == 6 => "5.1",
+            Audio(c) if c.channels == 4 => "4.0",
+            Audio(c) if c.channels == 2 => "2.0",
+            Audio(c) if c.channels == 0 => "1.0",
+            _ => panic!("Failed to find channel info."),
+        }.to_string();
 
         writer.serialize(Movie {
             title, year, size, duration, res, bit_depth, v_codec, a_codec, channels, encoder, remux
         })?;
-
         // println!("Successfully finished logging {}", title);
     }
 
     writer.flush()?;
-
     Ok(())
 }
 
-fn get_encoder(title: &str) -> Option<&str> {
+fn get_encoder(title: &str) -> Option<String> {
     let enc_list = ["Tigole", "FraMeSToR", "Silence", "afm72", "DDR", "Bandi", "SAMPA", "3xO", "Joy", "RARBG", "SARTRE", "PHOCiS", "TERMiNAL", "PSA", "K1tKat", "FreetheFish", "Natty", "IchtyFinger", "BeiTai", "LEGi0N", "HDH", "HANDS", "GREENOTEA", "IWFM", "FRDS", "Ritaj", "Enthwar", "t3nzin", "EDG"];
 
     for enc in enc_list{
         if title.contains(enc){
-            return Some(enc);
+            return Some(String::from(enc));
         }
     }
     None
@@ -256,3 +310,8 @@ fn parse(directories: &Vec<PathBuf>) -> Vec<Movie> {
 }
 
 */
+
+    // ! Use to generate an example matroska type
+    // let f = std::fs::File::open(&directories[99]).unwrap();
+    // let matroska = Matroska::open(&f).unwrap();
+    // println!("{:?}", matroska);
